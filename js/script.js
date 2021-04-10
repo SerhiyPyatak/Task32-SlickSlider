3119087fb0dd3598a1b72973bd98da6066b1ae9a
SerhiyPyatak <serhiypyatak@yahoo.com>
2021-04-10 11:52:31 +0300

    This is initial commit which adds slider

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(  document   ).ready(function(){
+	const prevArrow = '<div class="arrow-previous"></div>';
+	const       nextArrow = '<div class="arrow-next"></div>';
+	$(      '.slider'     ).slick(
+		{
+			infinite: true,
+  			slidesToShow: 4,
+			slidesToScroll: 4,
+			prevArrow,
+			nextArrow
+	}
+	);
+  });
