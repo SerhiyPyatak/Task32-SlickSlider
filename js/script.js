9cacd62f3561143cb41553ea829d5bb724eab966
SerhiyPyatak <serhiypyatak@yahoo.com>
2021-04-10 18:03:27 +0300

    This commit states at 10.04.21 18:03

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 "use strict";
-$(  document   ).ready(function(){
-	const prevArrow = '<div class="arrow-previous"></div>';
-	const       nextArrow = '<div class="arrow-next"></div>';
-	$(      '.slider'     ).slick(
-		{
-			infinite: true,
-  			slidesToShow: 4,
-			slidesToScroll: 4,
-			prevArrow,
-			nextArrow
-	}
-	);
-  });
+/*import $ from '../node_modules/jquery/dist/jquery.min.js';*/
+
+$(document).ready(function() {
+  const prevArrow = '<div class="arrow-previous"></div>';
+  const nextArrow = '<div class="arrow-next"></div>';
+  $('.slider').slick(
+    {
+      infinite: true,
+  	  slidesToShow: 4,
+      slidesToScroll: 4,
+      prevArrow,
+      nextArrow
+    }
+  );
+});
